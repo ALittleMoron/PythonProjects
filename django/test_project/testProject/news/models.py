c7a4f453e010b9562b9684b959ebf4a4c926fff0
ALittleMoron <dima.lunev14@gmail.com>
2021-03-07 17:18:15 +0300

    Создал модель Новости

--- django/test_project/testProject/news/models.py
+++ django/test_project/testProject/news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    """ Модель новостей. """
+    title = models.CharField(max_length=150)
+    context = models.TextField(blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+
