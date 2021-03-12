99715ba6ae5f3e277d53aaa927dbee5b3a52344b
ALittleMoron <dima.lunev14@gmail.com>
2021-03-12 19:31:07 +0300

    изменил модель, добавил альтернативные названия полей и мета-класс для новости

--- django/test_project/testProject/news/models.py
+++ django/test_project/testProject/news/models.py
@@ -3,9 +3,18 @@ from django.db import models
 
 class Article(models.Model):
     """ Модель новостей. """
-    title = models.CharField(max_length=150)
-    context = models.TextField(blank=True)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-    is_published = models.BooleanField(default=False)
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Создано')
+    updated_at = models.DateField(auto_now=True, verbose_name='Обновлено')
+    is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
 
+
+    def __str__(self):
+        return '{} - {} - {}'.format(self.title, self.content, self.created_at)
+
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+        ordering = ['-created_at']
