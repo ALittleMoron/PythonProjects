ff9a4afb7a1974eb2a7ca0e661486dbdccf5b302
ALittleMoron <dima.lunev14@gmail.com>
2021-03-13 22:07:29 +0300

    Новая модель категории новости, связь с ней через внешний ключ и миграция на новую модель

--- django/test_project/testProject/news/models.py
+++ django/test_project/testProject/news/models.py
@@ -8,13 +8,28 @@ class Article(models.Model):
     created_at = models.DateField(auto_now_add=True, verbose_name='Создано')
     updated_at = models.DateField(auto_now=True, verbose_name='Обновлено')
     is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
 
 
     def __str__(self):
-        return '{} - {} - {}'.format(self.title, self.content, self.created_at)
+        return self.title
 
 
     class Meta:
         verbose_name = "Новость"
         verbose_name_plural = "Новости"
         ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+    
+    
+    def __str__(self):
+        return self.title
+
+    
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['title']
