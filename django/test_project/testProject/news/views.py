1b289c6be0e65c8e386fd2ce97a97b357286e111
ALittleMoron <dima.lunev14@gmail.com>
2021-03-14 18:57:31 +0300

    вьюшка под переделанный шаблон

--- django/test_project/testProject/news/views.py
+++ django/test_project/testProject/news/views.py
@@ -6,4 +6,4 @@ from .models import Article
 
 def index(request):
     news = Article.objects.all()
-    return render(request, 'news/index.html', {'news': news, 'title': 'Все новости'})
+    return render(request, 'news/news.html', {'news': news, 'title': 'Все новости'})
