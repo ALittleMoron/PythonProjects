30fb558022610b16048181d78fb0009d1ff109bf
ALittleMoron <dima.lunev14@gmail.com>
2021-03-15 22:15:01 +0300

    Добавил функцию отображения для новостей определенной категории

--- django/test_project/testProject/news/views.py
+++ django/test_project/testProject/news/views.py
@@ -1,9 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Article
+from .models import Article, Category
 
 
 def index(request):
-    news = Article.objects.all()
-    return render(request, 'news/news.html', {'news': news, 'title': 'Все новости'})
+    content = {
+        'title': 'Все новости',
+        'news': Article.objects.all(),
+        'categories': Category.objects.all(),
+    }
+    return render(request, 'news/news.html', content)
+
+
+def get_category(request, category_id):
+    content = {
+        'title': Category.objects.get(id=category_id),
+        'news': Article.objects.filter(category_id=category_id, is_published=True),
+        'categories': Category.objects.all(),
+    }
+    return render(request, 'news/news.html', content)
