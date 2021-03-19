dad0c127bc38f1616c83ba030163ceb4be40e86c
ALittleMoron <dima.lunev14@gmail.com>
2021-03-19 20:06:16 +0300

    остальные изменения согласно предыдущим коммитам

--- django/test_project/testProject/news/views.py
+++ django/test_project/testProject/news/views.py
@@ -8,16 +8,14 @@ def index(request):
     content = {
         'title': 'Все новости',
         'news': Article.objects.all(),
-        'categories': Category.objects.all(),
     }
     return render(request, 'news/news.html', content)
 
 
-def get_category(request, category_id):
+def get_news_by_category(request, category_id):
     content = {
         'title': Category.objects.get(id=category_id),
         'news': Article.objects.filter(category_id=category_id, is_published=True),
-        'categories': Category.objects.all(),
     }
     return render(request, 'news/news.html', content)
 
